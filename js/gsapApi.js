17e5410c73bfb02c8ba040d6519e90772d4d2212
esleptsova <Unik.SE>
2020-11-21 14:25:54 +0000

    main.js refactored

---
+++ js/gsapApi.js
@@ -0,0 +1,3 @@
+gsap.registerPlugin(TextPlugin);
+gsap.to(".home__name--first", { duration: 1.5, text: "Kristi", ease: "none" });
+gsap.to(".home__name--last", { delay: 1.5, duration: 1.5, text: "Hines", ease: "none" });
--- js/main.js
+++ js/main.js
@@ -24,7 +24,5 @@ function toggleMenu() {
   }
 }
 
-gsap.registerPlugin(TextPlugin);
-gsap.to(".home__name--first", { duration: 1.5, text: "Kristi", ease: "none" });
-gsap.to(".home__name--last", { delay: 1.5, duration: 1.5, text: "Hines", ease: "none" });
+
 
