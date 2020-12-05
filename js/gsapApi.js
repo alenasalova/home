74a31e737d6395920972311b30ebc2f45fe276ab
esleptsova <Unik.SE>
2020-12-05 12:56:50 +0000

    content updated

--- js/gsapApi.js
+++ js/gsapApi.js
@@ -1,3 +1,4 @@
 gsap.registerPlugin(TextPlugin);
-gsap.to(".home__intro_1", { duration: 2, text: "Копирайтер.", ease: "none" });
-gsap.to(".home__intro_2", { delay: 2, duration: 1.5, text: "Пишу тексты и статьи на заказ.", ease: "none" });
+gsap.to(".home__intro_1", { delay: 1, duration: 2, text: "Копирайтер.", ease: "none" });
+gsap.to(".home__intro_2", { delay: 3, duration: 2.5, text: "Пишу тексты для блогов и соцсетей,", ease: "none" });
+gsap.to(".home__intro_3", { delay: 5.5, duration: 2.5, text: " делаю описания товаров и услуг.", ease: "none" });
